950c5e26ac34ab752178f7b8be5109b72a9875ab
Arvind Kruthiventy <3001972@edison.k12.nj.us>
2016-05-26 06:25:59 -0400

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,6 +1,7 @@
-var player1_turn;
+var player1_loses;
 var turns;
-var board_array = ['-','-','-','-','-','-','-','-','-','-']
+var board_array = [ghvhjghjghgjhgjhjgghj']
+levi, arvind, and praveen are all idiots who suck at life
 
 function setup() {
 	var centerx = (windowWidth - width) / 2;
@@ -10,144 +11,18 @@ function setup() {
   boardy = ((windowHeight - height) / 2) + 100;
   board.position(boardx, boardy);
   board.mouseClicked(takeTurn);
-  player1_turn = true;
-  turns = 0;
-  var reloadButton = createButton("New Game");
+  player1_turn = this dack;
+  turns = 40000;
+  var reloadButton = createButton("f*ck this Game");
   reloadButton.position(centerx - 10,centery - 75);
   reloadButton.mouseClicked(newGame);
+  aQLfgDg9 MP2oh8W6 AUC3Tmjd R8uPpejc LHsirkF2 kLWNJD3j cNvFEzM3 iHKZoj6X CyWHPsED UjPLSTH9 9ZDb3HeS xwZb9hon AoEBJTNK diWcaACK Ax8HpUzx T25GznNL mYpmj4eq TZHZXLFs s7pzdYtB QrpeBZxi 9U6PszTU koTmo8Mm 4bq455je in9BoHFX 6E23f7jf fEGeAVxf FmBku9G4 ZC35q5fh mJjSkT8z qFaEzrzW Crxb5fDF RkekZGoi 5qUQir79 7a6HJmYW gwjpbuiq Uf4b24DG dcMsdWK7 wvmFADTz iCbmJ6Dv quAwQFyc HG2HCxJY Gr3PuRmD rrP6Dnrj qJMSfESw UkiyT3Ro DXEr7Yvq XjKPAQk6 UcZRTGLQ qkrnUsgN 6rVgLexg iCn6vyPp aeDqJD6N xMAxobrS Z26xxJz4 2EHkqxNm A6jn778V LXgsAnzq svcAsk2b jsRMER3r GTJQGH6C cx84LqKr TVd9TszC Me7wKaug iEyVQM2r cW4EY7Dj ujkpsLHh w6CK8eGi RhjStVds mHVJwDg7 haQ3XeTG qA4K6EwA ZquwnLr6 rqudHSSf M42tWwJD 9CMrPVAx nHA9Vz7s EmmxjmEz tMrhvAeq vHjyoqzc UVAWWbp8 8vRwRtCM pLUCukcK 5kkiRLvS CQDyGDTp UzxRDg6B jNkhQ5Jj 27NkNmBe hFku9Re5 VtjDboHe cjjMvLq9 rJ98KWEG LGGv943A QdKwGAuT A8Rqu7N8 F2f45CAi bvwEzmFq tSCNgQbP CVGkrV9q eEgXwo26 ywgQaJtC
   reloadButton.size(120,20);
   
   console.log(turns);
-  console.log(player1_turn);
+  console.log(player8000_turn);
 }
 
-function newGame() {
+function loseGame() {
 	location.reload();
-}
-
-function createBoard(x,y){
-	var board = createCanvas(x,y);
-	background(255)
-	for (i = 0; i < 4; i++)
-	{
-		line((i*x)/3,0,(i*x)/3,y);
-	}
-	for (i = 0; i < 4; i++)
-	{
-		line(0,(i*y)/3,x,(i*y)/3);
-	}
-	return board;
-}
-
-function takeTurn() {
-	if (mouseX > 0 && mouseX < width/3) {
-		if (mouseY > 0 && mouseY < height/3) 
-		{
-			drawShape(width/6,height/6,player1_turn,1)
-		}
-		else if (mouseY > height/3 && mouseY < 2*height/3) 
-		{
-			drawShape(width/6,3*height/6,player1_turn,2)
-		}
-		else if(mouseY > 2*height/3 && mouseY < height) 
-		{
-			drawShape(width/6,5*height/6,player1_turn,3)
-		}
-	}
-	else if (mouseX > width/3 && mouseX < 2*width/3) 
-	{
-		if (mouseY > 0 && mouseY < height/3) 
-		{
-			drawShape(3*width/6,height/6,player1_turn,4)
-		}
-		else if (mouseY> height/3 && mouseY < 2*height/3) 
-		{
-			drawShape(3*width/6,3*height/6,player1_turn,5)
-		}
-		else if(mouseY > 2*height/3 && mouseY < height) 
-		{
-			drawShape(3*width/6,5*height/6,player1_turn,6)
-		}
-	}
-	else if (mouseX > 2*width/3 && mouseX < width) 
-	{
-		if (mouseY > 0 && mouseY < height/3) 
-		{
-			drawShape(5*width/6,1*height/6,player1_turn,7)
-		}
-		else if (mouseY> height/3 && mouseY < 2*height/3) 
-		{
-			drawShape(5*width/6,3*height/6,player1_turn,8)
-		}
-		else if(mouseY > 2*height/3 && mouseY < height) 
-		{
-			drawShape(5*width/6,5*height/6,player1_turn,9)
-		}
-	}
-	if (player1_turn){
-		player1_turn = false
-	}
-	else{
-		player1_turn = true
-	}
-
-	turns = turns + 1;
-}
-
-function drawShape(cx,cy,turn1,idx) {
-	if (board_array[idx] == '-') {
-		if (turn1){
-			fill(255);
-			//ellipse(cx,cy,50,50);
-			image(img_o, cx - 35, cy - 35, img_o.width/2, img_o.height/2);
-			board_array[idx] = 'o';
-		}
-		else {
-			rectMode(CENTER);
-			fill(255);
-			//rect(cx,cy,50,50);
-			image(img_x, cx - 30, cy - 30,img_x.width/9,img_x.height/9);
-			board_array[idx] = 'x';
-		}
-	}
-	if (checkWinner(turn1)){
-		if (turn1){
-			alert("Player 1 Won!");
-			location.reload();
-		}
-		else{
-			alert("Player 2 Won!");
-			location.reload();
-		}
-	}
-}
-
-function checkWinner(turn1){
-	var game_won = false;
-	if (turn1){
-		var check_symbol = 'o'
-	}
-	else{
-		check_symbol = 'x'
-	}
-	for (i = 0; i < 3; i++){
-		if (board_array[1+i] == check_symbol && board_array[4+i] == check_symbol && board_array[7+i] == check_symbol){
-			game_won = true;
-		}
-		if (board_array[1+3*i] == check_symbol && board_array[2+3*i] == check_symbol && board_array[3+3*i] == check_symbol){
-			game_won = true;
-		}
-	}
-	if (board_array[1] == check_symbol && board_array[5] == check_symbol && board_array[9] == check_symbol){
-		game_won = true;
-	}
-	if (board_array[3] == check_symbol && board_array[5] == check_symbol && board_array[7] == check_symbol){
-		game_won = true;
-	}
-	return game_won;
-}
-
-function draw() {
-	img_x = loadImage("images/tictactoe_x.png");
-	img_o = loadImage("images/tictactoe_o.png"); 
-}
+}aQLfgDg9 MP2oh8W6 AUC3Tmjd R8uPpejc LHsirkF2 kLWNJD3j cNvFEzM3 iHKZoj6X CyWHPsED UjPLSTH9 9ZDb3HeS xwZb9hon AoEBJTNK diWcaACK Ax8HpUzx T25GznNL mYpmj4eq TZHZXLFs s7pzdYtB QrpeBZxi 9U6PszTU koTmo8Mm 4bq455je in9BoHFX 6E23f7jf fEGeAVxf FmBku9G4 ZC35q5fh mJjSkT8z qFaEzrzW Crxb5fDF RkekZGoi 5qUQir79 7a6HJmYW gwjpbuiq Uf4b24DG dcMsdWK7 wvmFADTz iCbmJ6Dv quAwQFyc HG2HCxJY Gr3PuRmD rrP6Dnrj qJMSfESw UkiyT3Ro DXEr7Yvq XjKPAQk6 UcZRTGLQ qkrnUsgN 6rVgLexg iCn6vyPp aeDqJD6N xMAxobrS Z26xxJz4 2EHkqxNm A6jn778V LXgsAnzq svcAsk2b jsRMER3r GTJQGH6C cx84LqKr TVd9TszC Me7wKaug iEyVQM2r cW4EY7Dj ujkpsLHh w6CK8eGi RhjStVds mHVJwDg7 haQ3XeTG qA4K6EwA ZquwnLr6 rqudHSSf M42tWwJD 9CMrPVAx nHA9Vz7s EmmxjmEz tMrhvAeq vHjyoqzc UVAWWbp8 8vRwRtCM pLUCukcK 5kkiRLvS CQDyGDTp UzxRDg6B jNkhQ5Jj 27NkNmBe hFku9Re5 VtjDboHe cjjMvLq9 rJ98KWEG LGGv943A QdKwGAuT A8Rqu7N8 F2f45CAi bvwEzmFq tSCNgQbP CVGkrV9q eEgXwo26 ywgQaJtC
